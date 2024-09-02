cc0f2295303ba78b97a6ce1feefe8dbb5f33c8af
Neisan <nathan.maillot@epitech.eu>
2024-09-02 16:28:51 +0400

    feat: add Statistic graphic logic

--- frontend/src/data.tsx
+++ frontend/src/data.tsx
@@ -1,12 +1,7 @@
 import { HiUser } from "react-icons/hi";
 
 export const users = [
-    [
-        { href: "#", icon: HiUser, content: "User 1" },
-        { href: "#", icon: HiUser, content: "User 2" },
-        { href: "#", icon: HiUser, content: "User 3" },
-    ],
-    [
-        { href: "#", avatar: "kbo.png", content: "image" },
-    ],
+    { href: "#", icon: HiUser, content: "User 1", meetings: [35, 51, 15, 60] },
+    { href: "#", icon: HiUser, content: "User 2", meetings: [51, 6, 49, 30] },
+    { href: "#", icon: HiUser, content: "User 3", meetings: [35, 60, 15, 25] },
 ];
