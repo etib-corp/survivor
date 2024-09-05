703d9c7cd271140d50b83512ee44e1b12e1215e9
Lauret Benjamin <rstraif@Benjos-MacBook-Pro.local>
2024-09-05 21:07:52 +0400

    feat: add CustomersCompatibility full responsive

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Statistics from './pages/Statistics';
 import Account from './pages/Coaches';
 import Customers from './pages/Customers';
 import Tips from './pages/Tips';
+import Compatibility from './pages/Compatibility';
 
 
 function App() {
@@ -14,11 +15,12 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
-          <Route path="/Account" element={<Account/>}></Route>
+          {/* <Route path="/Account" element={<Account/>}></Route>
           <Route path="/Customers" element={<Customers/>}></Route>
           <Route path="/Profiles" element={<Profiles/>}></Route>
           <Route path="/Statistics" element={<Statistics/>}></Route>
-          <Route path="/Tips" element={<Tips/>}></Route>
+          <Route path="/Tips" element={<Tips/>}></Route> */}
+          <Route path="/Compatibility" element={<Compatibility/>}></Route>
         </Routes>
       </div>
     </main>
---
+++ frontend/src/pages/Compatibility.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+import { Dropdown } from "flowbite-react";
+import CustomersCompatibility from '../components/Customers/CustomersCompatibility';
+import Customer from '../types/Customer';
+import CustomerService from '../services/CustomerService';
+import ETIBNavBar from '../components/ETIBNavBar';
+
+export default function Compatibility() {
+
+    const [properties, setProperties] = useState({ page: "compatibility" });
+
+    const [customers, setCustomers] = useState<Customer[]>([]);
+    useEffect(() => {
+        CustomerService.getAll().then((response: any) => {
+            if (Array.isArray(response.data['hydra:member'])) {
+                setCustomers(response.data['hydra:member']);
+                console.log(response.data['hydra:member']);
+            } else {
+                console.log("Expected an array of customers but got:", response.data);
+            }
+        }).catch((e) => {
+            console.log(e);
+        });
+    }, []);
+
+    return (
+        <div className="overflow-x-hidden">
+            <ETIBNavBar properties={properties} OnChangeView={setProperties}/>
+            <CustomersCompatibility properties={customers} />
+        </div>
+    )
+}
