bfae1e9d4c912c5114b75176ee06be686045da1f
Neisan <nathan.maillot@epitech.eu>
2024-09-05 20:21:42 +0400

    fix: backend on Meeting Top Sources

--- frontend/src/components/Statistics/MeetingTopSources.tsx
+++ frontend/src/components/Statistics/MeetingTopSources.tsx
@@ -1,9 +1,50 @@
+import { useState, useEffect } from "react";
+
 import { HiChevronDown } from "react-icons/hi";
 
-import { Button, Dropdown} from "flowbite-react";
-import { PieChart } from "@mui/x-charts";
+import { Button, Dropdown } from "flowbite-react";
+import { PieChart, pieArcLabelClasses } from "@mui/x-charts";
 
-export default function MeetingTopSources () {
+import EncounterService from "../../services/EncounterService";
+
+export default function MeetingTopSources() {
+    const [selected, setSelected] = useState(30);
+    const [serie, setSerie] = useState<any>([]);
+    const today = new Date();
+
+    useEffect(() => {
+        EncounterService.getAll().then((response: any) => {
+            const encounters = response.data["hydra:member"];
+            let minDate = new Date();
+            let data = new Array();
+
+            minDate.setDate(today.getDate() - selected);
+
+            encounters.map((encounter: any) => {
+                let tmpDate = new Date(encounter.date);
+
+                if (tmpDate > minDate) {
+                    data.push(encounter.source);
+                }
+            });
+            const wordCount: { [key: string]: number } = {};
+
+            data.forEach(word => {
+                if (wordCount[word]) {
+                    wordCount[word]++;
+                } else {
+                    wordCount[word] = 1;
+                }
+            });
+
+            const sources = Object.keys(wordCount).map((word, index) => ({
+                id: index,
+                value: wordCount[word],
+                label: word
+            }));
+            setSerie(sources);
+        });
+    }, [selected]);
     return (
         <div className="flex flex-col bg-white border mx-[5%] md:mx-4 w-[90%] md:w-[45%] rounded-md">
             <div className="grid grid-cols-1 md:flex md:flex-row justify-between mx-4">
@@ -14,45 +55,53 @@ export default function MeetingTopSources () {
                 </div>
                 <div className="mt-auto mb-auto">
                     <div className="flex flex-row space-x-4 justify-center md:justify-normal">
-                    <Dropdown
-                    label=""
-                    dismissOnClick={false}
-                    renderTrigger={() =>
-                        <Button className="bg-transparent text-gray-700 border-gray-700 rounded-md mt-4">
-                            30 Days
-                            <HiChevronDown className="ml-2 h-5 w-5"/>
-                        </Button>
-                    }>
-                        <Dropdown.Item>Dashboard</Dropdown.Item>
-                        <Dropdown.Item>Settings</Dropdown.Item>
-                        <Dropdown.Item>Earnings</Dropdown.Item>
-                        <Dropdown.Item>Sign out</Dropdown.Item>
-                    </Dropdown>
+                        <Dropdown
+                            label=""
+                            dismissOnClick={false}
+                            renderTrigger={() =>
+                                <Button className="bg-transparent text-gray-700 border-gray-700 rounded-md mt-4">
+                                    Last {selected} Days
+                                    <HiChevronDown className="ml-2 h-5 w-5" />
+                                </Button>
+                            }>
+                            <Dropdown.Item onClick={() => (setSelected(7))}>
+                                1 Week
+                            </Dropdown.Item>
+                            <Dropdown.Item onClick={() => (setSelected(30))}>
+                                1 Month
+                            </Dropdown.Item>
+                            <Dropdown.Item onClick={() => (setSelected(90))}>
+                                3 Months
+                            </Dropdown.Item>
+                            <Dropdown.Item onClick={() => (setSelected(365))}>
+                                1 Year
+                            </Dropdown.Item>
+                        </Dropdown>
                     </div>
                 </div>
             </div>
-            <div className="flex justify-center w-[90%] md:w-[40%] mx-auto">
-                <PieChart
-                    margin={{ bottom: 50, right: 75, left: 75 }}
+            <div className="flex flex-col w-[90%] md:w-[40%] mx-auto">
+                <PieChart className="text-xl text-white"
+                    slotProps={{
+                        legend: {
+                            hidden: true,
+                        }
+                    }}
                     series={[
                         {
-                            data: [
-                                { id: 0, value: 10, label: 'series A' },
-                                { id: 1, value: 15, label: 'series B' },
-                                { id: 2, value: 20, label: 'series C' },
-                            ],
+                            arcLabel: (item) => `${item.label}`,
+                            arcLabelMinAngle: 35,
+                            arcLabelRadius: '60%',
+                            data: serie,
                             innerRadius: 50
                         },
                     ]}
                     height={400}
-                    slotProps={{
-                        legend: {
-                            direction: 'row',
-                            position: { vertical: 'bottom', horizontal: 'middle' },
-                            padding: 20,
-                        }
-                    }}
+                    width={400}
+
                 />
+                <div>
+                </div>
             </div>
         </div>
     )
