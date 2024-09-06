8c01deccb487e64dbf1d471b06b4fc9fdf7903b6
Neisan <nathan.maillot@epitech.eu>
2024-09-06 17:47:25 +0400

    fix: data on profile side

--- frontend/src/components/Customers/ProfileSide.tsx
+++ frontend/src/components/Customers/ProfileSide.tsx
@@ -1,15 +1,40 @@
+import { useEffect, useState } from "react";
+
 import { HiOutlineArchive, HiOutlineMail } from "react-icons/hi";
 
 import { Avatar } from "flowbite-react";
+import EncounterService from "../../services/EncounterService";
 
 const ProfileSide: React.FC<{ properties: any }> = ({ properties }) => {
+    const [positives, setPositives] = useState(0);
+
+    useEffect(() => {
+        function fetchEncounters() {
+            return Promise.all(properties.encounters.map((encounter: string) => {
+                const id = parseInt(encounter.split("/").pop() as string);
+                return EncounterService.get(id);
+            }));
+        }
+
+        fetchEncounters().then((responses) => {
+            let count = 0;
+            responses.map((response) => {
+                const data = response.data;
+                if (data.rating > 3) {
+                    count++;
+                }
+            });
+            setPositives(count);
+        });
+    }, []);
+    console.log(properties);
     return (
         <div className="flex flex-col bg-white border mr-8 w-[25%] rounded-md">
             <div className="flex flex-col bg-white border py-8">
-                <Avatar size="lg" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded/>
+                <Avatar size="lg" img={process.env.REACT_APP_PICTURES_URL + "/customers/" + properties.id + ".png"} rounded/>
                 <div className="flex flex-col p-4 mx-auto text-center">
                     <h1 className="text-2xl font-bold">
-                        {properties.name}
+                        {properties.name + " " + properties.surname}
                     </h1>
                 </div>
             </div>
@@ -30,18 +55,10 @@ const ProfileSide: React.FC<{ properties: any }> = ({ properties }) => {
                 </div>
                 <div className="flex flex-col text-center w-[30%]">
                     <h1 className="text-2xl font-bold">
-                        {properties.positives}
+                        {positives}
                     </h1>
                     <p className="text-gray-500">
                         Positives
-                    </p>
-                </div>
-                <div className="flex flex-col text-center w-[30%]">
-                    <h1 className="text-2xl font-bold">
-                        {properties.inprogress}
-                    </h1>
-                    <p className="text-gray-500">
-                        In Progress
                     </p>
                 </div>
             </div>
@@ -75,18 +92,18 @@ const ProfileSide: React.FC<{ properties: any }> = ({ properties }) => {
                 </div>
                 <div className="flex flex-col py-2">
                     <p className="text-gray-500">
-                        Last Activity:
+                        Gender
                     </p>
                     <p className="">
-                        {properties.lastActivity}
+                        {properties.gender}
                     </p>
                 </div>
                 <div className="flex flex-col py-2">
                     <p className="text-gray-500">
-                        Coach:
+                        Astrological Sign
                     </p>
                     <p className="">
-                        {properties.coach}
+                        {properties.astrologicalSign}
                     </p>
                 </div>
             </div>
