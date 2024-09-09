8216450c60bb4e1492895f9f2b606d52595a25fe
Lauret Benjamin <rstraif@Benjos-MacBook-Pro.local>
2024-09-09 19:47:06 +0400

    feat: add card to elearning page

--- frontend/src/pages/Elearning.tsx
+++ frontend/src/pages/Elearning.tsx
@@ -1,3 +1,4 @@
+import ETIBCard from "../components/ETIBCard";
 import ETIBNavBar from "../components/ETIBNavBar";
 import { useState } from "react";
 
@@ -8,9 +9,17 @@ function Elearning() {
     <div className="overflow-x-hidden">
       <ETIBNavBar properties={props} OnChangeView={setProps} />
         <div className="flex">
-            <h1 className="text-2xl font-bold text-center mt-10">
+            <h1 className="text-4xl font-bold mt-8 ml-4">
                 E-Learning Page
             </h1>
+        </div>
+        <div className="flex justify-center items-center h-screen pb-[30%]">
+            <div className="grid grid-cols-1 mt-12 sm:grid-cols-2 md:grid-cols-4 md:mt-0 gap-10">
+                <ETIBCard title="E-Learning" subtitle="First lesson" buttonTitle="Read more" />
+                <ETIBCard title="E-Learning" subtitle="Second lesson" buttonTitle="Read more" />
+                <ETIBCard title="E-Learning" subtitle="Third lesson" buttonTitle="Read more" />
+                <ETIBCard title="E-Learning" subtitle="Fourth lesson" buttonTitle="Read more" />
+            </div>
         </div>
     </div>
   );
