4c295ec0c1dfba40876e845e88b67bd6923a88a4
Neisan <nathan.maillot@epitech.eu>
2024-09-10 11:16:46 +0400

    fix: dowload image

--- frontend/src/http-common.tsx
+++ frontend/src/http-common.tsx
@@ -4,6 +4,7 @@ export default axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
         "Content-type": "application/json",
-        "Authorization": "Bearer " + localStorage.getItem("authToken")
+        "Authorization": "Bearer " + localStorage.getItem("authToken"),
+        "Accept": "*/*",
     }
 });
