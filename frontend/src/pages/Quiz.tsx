58f42289dd6c9706ba43a89fc22f65356312f927
Neisan <nathan.maillot@epitech.eu>
2024-09-11 23:34:29 +0400

    feat: add real css on backend-dev-frontend-like elearning

--- frontend/src/pages/Quiz.tsx
+++ frontend/src/pages/Quiz.tsx
@@ -7,6 +7,8 @@ import { Button } from "flowbite-react";
 import ETIBQuestion from "../components/ETIBQuestion";
 import { useNavigate } from "react-router-dom";
 import QuizService from "../services/QuizService";
+import { buttonOutlineTheme } from "../themes";
+import { HiArrowLeft } from "react-icons/hi";
 
 function Quiz() {
     const [props, setProps] = useState({ page: "elearning" })
@@ -40,35 +42,41 @@ function Quiz() {
     }
 
     return (
-        <div className="overflow-x-hidden h-screen bg-pink-500">
+        <div className="overflow-x-hidden h-screen">
             <ETIBNavBar properties={props} OnChangeView={setProps} />
-            {quizItem === null ?
-                <div className="flex flex-col justify-center p-5 mx-[10vh]">
-                    <div className="flex items-center justify-between">
-                        <h1 className="text-4xl font-bold py-3">
-                            Quiz
-                        </h1>
-                        {privileges &&
-                            <Button className="float-end" onClick={() => {
-                                navigate('/Quiz/Add');
+            {
+                quizItem === null ?
+                    <div className="flex flex-col justify-center p-5 mx-[10vh]">
+                        <div className="flex items-center justify-between">
+                            <h1 className="text-4xl font-bold py-3">
+                                Quiz
+                            </h1>
+                            {privileges &&
+                                <Button className="float-end" onClick={() => {
+                                    navigate('/Quiz/Add');
+                                }
+                                }>Add QUIZ</Button>
                             }
-                            }>Add QUIZ</Button>
-                        }
+                        </div>
                     </div>
-                </div> :
-                <div className="">
-                    <div className="p-5 mx-[10vh]">
-                        <h1 className="text-4xl font-bold">{quizItem.title}</h1>
-                        <Button className="float-end" onClick={() => {
-                            params.delete('q');
-                            window.location.href = '/Elearning';
-                        }
-                        }>Back</Button>
+                    :
+                    <div className="flex flex-col space-y-8">
+                        <div className="grid grid-cols-1 md:flex md:flex-row justify-between md:mt-8 ml-4 mr-4">
+                            <h1 className="text-4xl font-bold py-1">
+                                {quizItem.title}
+                            </h1>
+                            <Button className="my-auto" theme={buttonOutlineTheme} color="default" onClick={() => {
+                                params.delete('q');
+                                window.location.href = '/Elearning';
+                            }}>
+                                <HiArrowLeft className="mr-2 h-5 w-5" />
+                                Back
+                            </Button>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            <ETIBQuestion QuizItem={quizItem} />
+                        </div>
                     </div>
-                    <div className="flex items-center justify-center">
-                        <ETIBQuestion QuizItem={quizItem} />
-                    </div>
-                </div>
             }
         </div>
     )
