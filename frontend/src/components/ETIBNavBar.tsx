c543319f28b7c192da9eba640d5e143c3704f4bb
Neisan <nathan.maillot@epitech.eu>
2024-09-11 18:39:07 +0400

    fix: color on navbar

--- frontend/src/components/ETIBNavBar.tsx
+++ frontend/src/components/ETIBNavBar.tsx
@@ -1,12 +1,16 @@
 import { useNavigate } from "react-router-dom";
 
-import AuthContext, { useAuth } from "./AuthContext";
+import { useAuth } from "./AuthContext";
 
 import { Navbar, Dropdown, Avatar } from "flowbite-react";
-import { RiMessage2Line } from "react-icons/ri";
-import { useContext, useEffect, useState } from "react";
 
-import { jwtDecode } from "jwt-decode";
+const theme = {
+  link: {
+    active: {
+      on: "text-pinkT"
+    }
+  }
+};
 
 const ETIBNavBar: React.FC<{ properties: any, OnChangeView: (viewName: any) => void }> = ({ properties, OnChangeView }) => {
   const navigate = useNavigate();
@@ -61,7 +65,7 @@ const ETIBNavBar: React.FC<{ properties: any, OnChangeView: (viewName: any) => v
   };
 
   return (
-    <Navbar fluid rounded className="shadow-sm">
+    <Navbar fluid rounded className="shadow-sm" theme={theme}>
       <Navbar.Toggle />
       <Navbar.Brand>
         <span className="sm:text-lg text-sm self-center whitespace-nowrap dark:text-white">
@@ -77,15 +81,13 @@ const ETIBNavBar: React.FC<{ properties: any, OnChangeView: (viewName: any) => v
           }
         >
           <Dropdown.Header>
-            <span className="block text-sm">
+            <span className="block text-sm font-semibold">
               {name + " " + surname}
             </span>
             <span className="block truncate text-sm font-medium">
               {email}
             </span>
           </Dropdown.Header>
-          {/* <Dropdown.Item>Settings</Dropdown.Item> */}
-          {/* <Dropdown.Divider /> */}
           <Dropdown.Item onClick={handleSignOut}>
             Sign out
           </Dropdown.Item>
@@ -94,76 +96,76 @@ const ETIBNavBar: React.FC<{ properties: any, OnChangeView: (viewName: any) => v
       <Navbar.Collapse>
         {
           role.type === "admin" &&
-          <Navbar.Link className="focus:text-blue" active={properties.page === "dashboard"} onClick={() => { navigate("/Home"); OnChangeView({ page: "dashboard" }) }}>
+          <Navbar.Link className="focus:text-pinkT" active={properties.page === "dashboard"} onClick={() => { navigate("/Home"); OnChangeView({ page: "dashboard" }) }}>
             Dashboard
             {
               properties.page === "dashboard" &&
-              <div className="sm:visible invisible relative h-[3px] bg-blueT top-[1.15rem] top rounded-md" />
+              <div className="sm:visible invisible relative h-[3px] bg-pinkT top-[1.15rem] top rounded-md" />
             }
           </Navbar.Link>
         }
         {
           role.type === "admin" &&
-          <Navbar.Link className="focus:text-blue" active={properties.page === "coaches"} onClick={() => { navigate("/Coaches"); OnChangeView({ page: "coaches" }) }}>
+          <Navbar.Link className="focus:text-pinkT" active={properties.page === "coaches"} onClick={() => { navigate("/Coaches"); OnChangeView({ page: "coaches" }) }}>
             Coaches
             {
               properties.page === "coaches" &&
-              <div className="sm:visible invisible relative h-[3px] bg-blueT top-[1.15rem] top rounded-md" />
+              <div className="sm:visible invisible relative h-[3px] bg-pinkT top-[1.15rem] top rounded-md" />
             }
           </Navbar.Link>
         }
         {
           role.type !== "customer" &&
-          <Navbar.Link className="focus:text-blueT" active={properties.page === "customers"} onClick={() => { navigate("/Customers"); OnChangeView({ page: "customers" }) }}>
+          <Navbar.Link className="focus:text-pinkT" active={properties.page === "customers"} onClick={() => { navigate("/Customers"); OnChangeView({ page: "customers" }) }}>
             Customers
             {
               properties.page === "customers" &&
-              <div className="sm:visible invisible relative h-[3px] bg-blueT top-[1.15rem] top rounded-md" />
+              <div className="sm:visible invisible relative h-[3px] bg-pinkT top-[1.15rem] top rounded-md" />
             }
           </Navbar.Link>
         }
         {
           role.type !== "customer" &&
-          <Navbar.Link className="focus:text-blueT" active={properties.page === "tips"} onClick={() => { navigate("/Tips"); OnChangeView({ page: "tips" }) }}>
+          <Navbar.Link className="focus:text-pinkT" active={properties.page === "tips"} onClick={() => { navigate("/Tips"); OnChangeView({ page: "tips" }) }}>
             Tips
             {
               properties.page === "tips" &&
-              <div className="sm:visible invisible relative h-[3px] bg-blueT top-[1.15rem] top rounded-md" />
+              <div className="sm:visible invisible relative h-[3px] bg-pinkT top-[1.15rem] top rounded-md" />
             }
           </Navbar.Link>
         }
         {
           role.type !== "customer" &&
-          <Navbar.Link className="focus:text-blueT" active={properties.page === "events"} onClick={() => { navigate("/Events"); OnChangeView({ page: "events" }) }}>
+          <Navbar.Link className="focus:text-pinkT" active={properties.page === "events"} onClick={() => { navigate("/Events"); OnChangeView({ page: "events" }) }}>
             Events
             {
               properties.page === "events" &&
-              <div className="sm:visible invisible relative h-[3px] bg-blueT top-[1.15rem] top rounded-md" />
+              <div className="sm:visible invisible relative h-[3px] bg-pinkT top-[1.15rem] top rounded-md" />
             }
           </Navbar.Link>
         }
-        <Navbar.Link className="focus:text-blueT" active={properties.page === "wardrobe"} onClick={() => { navigate("/Wardrobe"); OnChangeView({ page: "wardrobe" }) }}>
+        <Navbar.Link className="focus:text-pinkT" active={properties.page === "wardrobe"} onClick={() => { navigate("/Wardrobe"); OnChangeView({ page: "wardrobe" }) }}>
           Wardrobe
           {
             properties.page === "wardrobe" &&
-            <div className="sm:visible invisible relative h-[3px] bg-blueT top-[1.15rem] top rounded-md" />
+            <div className="sm:visible invisible relative h-[3px] bg-pinkT top-[1.15rem] top rounded-md" />
           }
         </Navbar.Link>
         {
           role.type !== "customer" &&
-          <Navbar.Link className="focus:text-blueT" active={properties.page === "compatibility"} onClick={() => { navigate("/Compatibility"); OnChangeView({ page: "compatibility" }) }}>
+          <Navbar.Link className="focus:text-pinkT" active={properties.page === "compatibility"} onClick={() => { navigate("/Compatibility"); OnChangeView({ page: "compatibility" }) }}>
             Compatibility
             {
               properties.page === "compatibility" &&
-              <div className="sm:visible invisible relative h-[3px] bg-blueT top-[1.15rem] top rounded-md" />
+              <div className="sm:visible invisible relative h-[3px] bg-pinkT top-[1.15rem] top rounded-md" />
             }
           </Navbar.Link>
         }
-        <Navbar.Link className="focus:text-blueT" active={properties.page === "elearning"} onClick={() => { navigate("/Elearning"); OnChangeView({ page: "elearning" }) }}>
+        <Navbar.Link className="focus:text-pinkT" active={properties.page === "elearning"} onClick={() => { navigate("/Elearning"); OnChangeView({ page: "elearning" }) }}>
           E-learning
           {
             properties.page === "elearning" &&
-            <div className="sm:visible invisible relative h-[3px] bg-blueT top-[1.15rem] top rounded-md" />
+            <div className="sm:visible invisible relative h-[3px] bg-pinkT top-[1.15rem] top rounded-md" />
           }
         </Navbar.Link>
       </Navbar.Collapse>
